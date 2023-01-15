e2f49eb58e3e4d8ffc3adbe6ec91d58e8246b03f
SACHIN_H_S <46858566+SACHINHS04@users.noreply.github.com>
2023-01-15 17:17:19 +0530

    Create MNIST_model_app.py

---
+++ MNIST_model_app.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import torch
+from PIL import Image
+
+# Load the trained model's state dictionary
+model = Net()
+model.load_state_dict(torch.load('mnist_model.pth'))
+model.eval()
+
+def predict(image):
+    # Preprocess the image
+    image = Image.open(image)
+    image = transforms.ToTensor()(image)
+    image = image.view(-1, 28 * 28)
+
+    # Pass the image through the model
+    output = model(image)
+    _, prediction = torch.max(output, 1)
+
+    return prediction
+
+st.set_page_config(page_title="MNIST model", page_icon=":guardsman:", layout="wide")
+
+st.title("MNIST model")
+
+file_image = st.file_uploader("Please upload your image", type=["jpg", "png"])
+
+if file_image:
+    image = Image.open(file_image)
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+    prediction = predict(file_image)
+    st.write("Prediction: ", prediction)
